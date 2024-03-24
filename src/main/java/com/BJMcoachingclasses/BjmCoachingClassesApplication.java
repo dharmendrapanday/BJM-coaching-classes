d1320a06a0ff0cd7a42f9bc305d6f98cd63562ea
Suryank08 <suryankmishra08@gmail.com>
2024-03-24 15:41:06 +0530

    adding spring boot application

---
+++ src/main/java/com/BJMcoachingclasses/BjmCoachingClassesApplication.java
@@ -0,0 +1,13 @@
+package com.BJMcoachingclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjmCoachingClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjmCoachingClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BJMcoachingclasses/BjmCoachingClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BJMcoachingclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjmCoachingClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
