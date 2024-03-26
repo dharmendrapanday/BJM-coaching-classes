563f6203def1f2935ccd3629baa9234c523b5395
Suryank08 <suryankmishra08@gmail.com>
2024-03-26 19:38:07 +0530

    Changes made by kukku

--- src/main/java/com/BJMcoachingclasses/controller/HomePageController.java
+++ src/main/java/com/BJMcoachingclasses/controller/HomePageController.java
@@ -1,5 +1,14 @@
 package com.BJMcoachingclasses.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
 public class HomePageController {
-    
+@RequestMapping("/")
+    public String homepage(){
+    return "homepage" ;
+}
+
 }
