0e356f9157db9d3287e5b224c13401c5e40ee88a
Suryank08 <suryankmishra08@gmail.com>
2024-03-28 12:56:44 +0530

    Added the model in spring application and added new student confirmation page

--- src/main/java/com/bjm/coaching/controller/SignupController.java
+++ src/main/java/com/bjm/coaching/controller/SignupController.java
@@ -1,12 +1,33 @@
 package com.bjm.coaching.controller;
 
+import com.bjm.coaching.model.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
     @GetMapping("/signup")
-    public String signup(){
+    public String signup(Model model){
+        //adding the attribute('student') in the model
+        model.addAttribute("student",new Student());
         return "signup";
+    }
+    @PostMapping("/process-student-form")
+    public String processStudentForm(@ModelAttribute("student")Student theStudent){
+
+        //Reteriving/accesing the 'student' attribute from the model by using(@ModelAttribute) (if attribute is not found in the model then it will create that attribute first)
+        // and then binding it theStudent object to use getter and setter method in java as below -->
+        System.out.println("Student Name :"+theStudent.getFirstName()+" "+ theStudent.getLastName());
+        System.out.println("Student Mob. num :"+theStudent.getMobileNumber());
+        System.out.println("Student Date of Birth :"+theStudent.getDob());
+        System.out.println("Student grade: "+theStudent.getGrade());
+        System.out.println("Student Stream: "+theStudent.getStream());
+        System.out.println("the Student school Name: "+theStudent.getSchoolName());
+        System.out.println("the student email: "+theStudent.getEmail());
+        System.out.println("the student password :"+theStudent.getPassword());
+        return "signup-confirmation";
     }
 }
---
+++ src/main/java/com/bjm/coaching/model/Student.java
@@ -0,0 +1,85 @@
+package com.bjm.coaching.model;
+
+public class Student {
+private String firstName;
+private String lastName;
+private String mobileNumber;
+private String  dob;
+private String grade;
+private String stream;
+private String schoolName;
+private String email;
+private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getStream() {
+        return stream;
+    }
+
+    public void setStream(String stream) {
+        this.stream = stream;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
